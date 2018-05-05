23cf2219d2cf80725fd632f9d9ef8dbb7f0e603d
Dylan Nissley <dylan.nissley@gmail.com>
2018-05-05 18:04:12 -0500

    Webpack: Integrate babel loader

--- webpack/src/print.js
+++ webpack/src/print.js
@@ -1,4 +1,9 @@
 export default function printMe() {
+  // use some syntax that is not supported in all browsers
+  // check how this file is transformed to make sure the syntax is replaced with polyfills
+  const a = { prop1: 'abc', prop2: 'def' };
+  const b = { ...a, prop1: 'overridden' };
   console.log('I get called from print.js');
+  console.log(JSON.stringify(b));
 }
 
